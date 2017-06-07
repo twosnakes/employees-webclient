8af7afd3758cf5c1a78435c26137aad61a5733d2
JackMarx <seriousfools@gmail.com>
2017-06-06 23:05:59 -0500

    create, update, destroy

--- app/controllers/employees_controller.rb
+++ app/controllers/employees_controller.rb
@@ -1,6 +1,6 @@
 class EmployeesController < ApplicationController
   def index
-    @employees = Unirest.get("localhost:3000/api/v2/employees.json").body
+    @employees = Unirest.get("localhost:3000/api/v1/employees.json").body
   end
 
   def new
@@ -9,16 +9,51 @@ class EmployeesController < ApplicationController
 
   def create
     employee = Unirest.post(
-                            "localhost:3000/api/v2/employees.json?first_name=#{params[:first_name]}&last_name=#{params[:last_name]}&email=#{params[:email]}",
+                            "localhost:3000/api/v1/employees.json",
                             headers: {
                                       "Accept" => "application/json"
-                                      }
+                                      },
+                            params: {
+                                     first_name: params[:first_name],
+                                     last_name: params[:last_name],
+                                     email: params[:email]
+                                    }
                             ).body
 
     redirect_to "/employees/#{employee["id"]}"
   end
 
   def show
-    @employee = Unirest.get("localhost:3000/api/v2/employees/#{params[:id]}.json").body
+    @employee = Unirest.get("localhost:3000/api/v1/employees/#{params[:id]}.json").body
+  end
+
+  def edit
+    @employee = Unirest.get("localhost:3000/api/v1/employees/#{params[:id]}.json").body
+  end
+
+  def update
+    employee = Unirest.patch(
+                            "localhost:3000/api/v1/employees/#{params["id"]}.json",
+                            headers: {
+                                      "Accept" => "application/json"
+                                      },
+                            params: {
+                                     first_name: params[:first_name],
+                                     last_name: params[:last_name],
+                                     email: params[:email]
+                                    }
+                            ).body
+
+    redirect_to "/employees/#{employee["id"]}"
+  end
+
+  def destroy
+    Unirest.delete(
+                   "localhost:3000/api/v1/employees/#{params["id"]}.json",
+                   headers: {
+                             "Accept" => "application/json"
+                             }
+                   ).body
+    redirect_to "/employees"
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,7 @@ Rails.application.routes.draw do
   get '/employees/new' => 'employees#new'
   post '/employees' => 'employees#create'
   get '/employees/:id' => 'employees#show'
+  get '/employees/:id/edit' => 'employees#edit'
+  patch '/employees/:id' => 'employees#update'
+  delete '/employees/:id' => 'employees#destroy'
 end
