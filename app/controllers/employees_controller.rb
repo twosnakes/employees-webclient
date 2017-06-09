0096c00386f622ea691ff7de4799a747ff1291c4
Zoe Lembeck <zlembeck@gmail.com>
2017-06-08 21:43:34 -0500

    restricted user access

--- app/controllers/employees_controller.rb
+++ app/controllers/employees_controller.rb
@@ -1,55 +1,50 @@
 class EmployeesController < ApplicationController
+  
   def index
-    @employees = Unirest.get("localhost:3000/api/v1/employees.json").body
-  end
+    @employees = Employee.all
+  end 
 
   def new
     
   end
 
   def create
-    employee = Unirest.post(
-                            "localhost:3000/api/v1/employees.json",
-                            headers: {
-                                      "Accept" => "application/json"
-                                      },
-                            params: {
-                                     first_name: params[:first_name],
-                                     last_name: params[:last_name],
-                                     email: params[:email]
-                                    }
-                            ).body
+
+
+    employee = Empoyee.create(
+                              first_name: params[:first_name],
+                              last_name: params[:last_name],
+                              email: params[:email]
+                            )
 
     redirect_to "/employees/#{employee["id"]}"
   end
 
   def show
-    @employee = Unirest.get("localhost:3000/api/v1/employees/#{params[:id]}.json").body
+    @employee = Employee.find(params[:id])
+
   end
 
   def edit
-    @employee = Unirest.get("localhost:3000/api/v1/employees/#{params[:id]}.json").body
+    @employee = Employee.find(params[:id])
   end
 
   def update
-    employee = Unirest.patch(
-                            "localhost:3000/api/v1/employees/#{params["id"]}.json",
-                            headers: {
-                                      "Accept" => "application/json"
-                                      },
-                            params: {
+    employee = Employee.find(params[:id])
+    employee.update(
+                                    parameters: {
                                      first_name: params[:first_name],
                                      last_name: params[:last_name],
                                      email: params[:email]
                                     }
                             ).body
 
-    redirect_to "/employees/#{employee["id"]}"
+    redirect_to "/employees/#{employee.id}"
   end
 
   def destroy
     Unirest.delete(
-                   "localhost:3000/api/v1/employees/#{params["id"]}.json",
+                   "#{ENV["API_HOST"]}/api/v1/employees/#{params["id"]}.json",
                    headers: {
                              "Accept" => "application/json"
                              }
